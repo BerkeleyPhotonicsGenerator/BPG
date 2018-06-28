e38cd1a223cba240d2ee56156918694237a73b63
pvnbhargava <pvnbhargava@eecs.berkeley.edu>
2018-06-27 22:51:45 -0700

    Supporting pipenv style installation

--- BPG/__init__.py
+++ BPG/__init__.py
@@ -1,4 +1,4 @@
 # Expose the LumericalGenerator class, which is the superclass of all lsf files
 
-from .LumericalProject import LumericalGenerator
+from .lumerical_generator import LumericalGenerator
 print('Successfully imported BPG')
