c7f56a50c2dffee2801a5030756bc2b7d680cd3f
pvnbhargava <pvnbhargava@eecs.berkeley.edu>
2018-10-17 11:03:35 -0700

    Modified logger to have default log file name be project name

--- BPG/logger.py
+++ BPG/logger.py
@@ -6,9 +6,9 @@ def setup_logger(logfile: str = 'bpg.log', verbose: bool = False) -> None:
     """ Configures the root logger so that all other loggers in BPG inherit from its properties """
 
     # Set up the initial basic config for the root logger
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    logger.handlers = []
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+    root_logger.handlers = []
 
     # Add a console handler
     out_handler = logging.StreamHandler()
@@ -18,24 +18,25 @@ def setup_logger(logfile: str = 'bpg.log', verbose: bool = False) -> None:
     else:
         out_handler.setLevel(logging.INFO)
 
-    # Add an output file to the root logger, overwrite the log file if it already exists
+    # Add an output file handler, overwrite the log file if it already exists
     file_handler = logging.FileHandler(logfile, 'w')
     file_handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter('[%(filename)s:%(lineno)d] %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
 
+    # Add a second output file handler for info only
     file_handler_info = logging.FileHandler(logfile + '_INFO', 'w')
     file_handler_info.setLevel(logging.INFO)
     file_handler_info.setFormatter(formatter)
 
-    logger.addHandler(file_handler)
-    logger.addHandler(file_handler_info)
-    logger.addHandler(out_handler)
+    # Add all handlers to root logger
+    root_logger.addHandler(file_handler)
+    root_logger.addHandler(file_handler_info)
+    root_logger.addHandler(out_handler)
 
     # Print out the current date and time
-    logger.info('##########################')
-    logger.info('Starting BPG Build')
+    root_logger.info('##########################')
+    root_logger.info('Starting BPG Build')
     time = datetime.now()
-    logger.info(str(time))
-    logger.info('##########################')
-
+    root_logger.info(str(time))
+    root_logger.info('##########################')
