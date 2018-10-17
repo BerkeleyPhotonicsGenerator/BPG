2610bc636bf8b3aa377c7878bbdbb2fca0768d7b
buchbinder <buchbinder@berkeley.edu>
2018-10-17 00:31:35 -0700

    Working on filtering logs.
    New dataprep debug log created, to catch only dataprep debug messages.
    Tests show dataprep debug statements make it to both logs, rather than to just the dedicated log

--- BPG/logger.py
+++ BPG/logger.py
@@ -2,15 +2,20 @@ import logging
 from datetime import datetime
 
 
-def setup_logger(logfile: str = 'bpg.log',
+def setup_logger(log_path: str,
+                 log_filename: str = 'bpg.log',
                  verbose: bool = False,
                  ) -> None:
     """
 
     Parameters
     ----------
-    logfile
-    verbose
+    log_path : str
+        The path to save the log files.
+    log_filename : str
+        The name of the primary output log file.
+    verbose : bool
+        True to output debug level messages to stdout. False to output info level messages to stdout.
 
     Returns
     -------
@@ -32,18 +37,22 @@ def setup_logger(logfile: str = 'bpg.log',
         out_handler.setLevel(logging.INFO)
 
     # Add an output file to the root logger, overwrite the log file if it already exists
-    file_handler = logging.FileHandler(logfile, 'w')
+    file_handler = logging.FileHandler(log_path + '/' + log_filename, 'w')
     file_handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter('[%(filename)s:%(lineno)d] %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
 
-    file_handler_info = logging.FileHandler(logfile + '_INFO', 'w')
+    # Add an output file to the root logger for INFO level logs and higher (debug can get messy)
+    file_handler_info = logging.FileHandler(log_path + '/' + log_filename + '_INFO', 'w')
     file_handler_info.setLevel(logging.INFO)
     file_handler_info.setFormatter(formatter)
 
     logger.addHandler(file_handler)
     logger.addHandler(file_handler_info)
     logger.addHandler(out_handler)
+    logger.addHandler(file_handler_info)
+    # Add filter to prevent dataprep debug logs from hitting the main logger
+    logger.addFilter(DataprepFilter())
 
     # Print out the current date and time
     logger.info('##########################')
@@ -53,9 +62,26 @@ def setup_logger(logfile: str = 'bpg.log',
     logger.info('##########################')
 
     # Set up a dataprep logger for dumping dataprep debug data
-    dataprep_logger = logging.getLogger(name='dataprep')
+    dataprep_logger = logging.getLogger('.dataprep')
     dataprep_logger.setLevel(logging.DEBUG)
     dataprep_logger.handlers = []
 
     # Add a file stream for the dataprep logger
-    dataprep_file_handler = logging.FileHandler()
+    dataprep_file_handler = logging.FileHandler(log_path + '/' 'dataprep_debug_dump.log', 'w')
+    dataprep_file_handler.setLevel(logging.DEBUG)
+    dataprep_file_handler.setFormatter(formatter)
+
+    dataprep_logger.addHandler(dataprep_file_handler)
+
+    # Print out the current date and time
+    dataprep_logger.info('##########################')
+    dataprep_logger.info('Starting BPG Build')
+    dataprep_logger.info('Initializing dataprep_debug_dump.log')
+    time = datetime.now()
+    dataprep_logger.info(str(time))
+    dataprep_logger.info('##########################')
+
+
+class DataprepFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord):
+        return not (record.levelno == logging.DEBUG and record.name == 'dataprep')
