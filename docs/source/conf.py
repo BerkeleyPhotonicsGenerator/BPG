918a71bd9d1a6feac42357bb67af0414d089141f
pvnbhargava <pvnbhargava@berkeley.edu>
2018-10-19 13:46:37 -0700

    Added skeleton sections for docs
    Added getting started guide, configuration file guide, dataprep guide, etc.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@
 #
 import os
 import sys
+from recommonmark.parser import CommonMarkParser
 sys.path.insert(0, os.path.abspath('../..'))
 
 
@@ -50,11 +51,11 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+source_parsers = {'.md': CommonMarkParser}
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
