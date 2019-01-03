e86fab0385a85b1463a22b5c406933031ec4d81f
Pavan Bhargava <pavan@ayarlabs.com>
2019-01-03 14:41:31 -0500

    Support adding to path from bag_config.yaml, paths are setup on import of BPG, bpg cli functional
    Support adding to path from bag_config.yaml, paths are setup on import of BPG, bpg cli functional

--- BPG/__init__.py
+++ BPG/__init__.py
@@ -1,15 +1,35 @@
-# Expose the LumericalGenerator class, which is the superclass of all lsf files
-from .lumerical.code_generator import LumericalDesignGenerator, LumericalSweepGenerator
+import os
+import sys
+from .template import PhotonicTemplateBase  # Expose PhotonicTemplateBase so that all generators can subclass it
+from .layout_manager import PhotonicLayoutManager  # Expose PLM to simplify BPG usage
 
-# Expose PhotonicTemplateBase so that all Generators can subclass it
-from .template import PhotonicTemplateBase
 
-# Expose PhotonicLayoutManager/PhotonicBagProject to encapsulate gds and lsf export
-from .layout_manager import PhotonicLayoutManager
-from .photonic_core import PhotonicBagProject
+def setup_environment():
+    """ Sets up python module search path from config file """
+    config = PhotonicLayoutManager.load_yaml(os.environ['BAG_CONFIG_PATH'])
+    # Add paths specified in config file
+    if 'path_setup' in config:
+        for path in config['path_setup']:
+            if path not in sys.path:
+                sys.path.append(path)
+                print(f'Adding {path} to python module search path')
 
-# Expose base shapes that can be drawn
-from . import objects
+
+def check_environment():
+    """ Checks that all required environment variables have been set """
+    env_list = [('BAG_WORK_DIR', 'Root directory where all generator folders are located'),
+                ('BAG_CONFIG_PATH', 'Path to yaml file containing configuration for BAG and BPG'),
+                ('BAG_FRAMEWORK', 'Path to BAG installation'),
+                ('BAG_TEMP_DIR', 'Path to where temporary files will be placed'),
+                ('BAG_TECH_CONFIG_DIR', 'Path to directory containing tech-specific configuration'),
+                ]
+
+    for var, description in env_list:
+        if var not in os.environ:
+            print(f'Environment variable {var} not set.\nDescription:{description}')
+
 
 __version__ = '0.4.0'
-print(f'Successfully imported BPG v{__version__}')
+print(f'Loaded BPG v{__version__}')
+check_environment()
+setup_environment()
