ad4a28d393a7cc1bbea04b5f69a60e246fdf8069
John Fini <johnmfini@gmail.com>
2019-03-01 00:17:07 -0800

    corrected geometry and template so that reflect flips signs of some rotations, and conversion correctly captures the order of reflect and rotate; comments and prints to be trimmed later

--- BPG/geometry.py
+++ BPG/geometry.py
@@ -634,16 +634,28 @@ class Transformable2D:
             is_cardinal : True if the angle is within error tolerance of being cardinal
             num_90deg : The number of 90 degree rotations (rounded down) that fit into the angle.
         """
-        num_90deg = int(np.floor(angle / (np.pi / 2)))
-        mod_angle = angle - num_90deg * (np.pi / 2)
+        if(mirrored): 
+            num_90deg = -int(np.floor(-angle / (np.pi / 2)))  
+            mod_angle = -(angle - num_90deg * (np.pi / 2))
+        else:
+            num_90deg = int(np.floor(angle / (np.pi / 2)))
+            mod_angle = angle - num_90deg * (np.pi / 2)
 
-        if 0 <= mod_angle < Transformable2D.SMALL_ANGLE_TOLERANCE:
+        if np.abs(mod_angle) < Transformable2D.SMALL_ANGLE_TOLERANCE:
             mod_angle = 0
             is_cardinal = True
-        elif np.pi / 2 > mod_angle > np.pi / 2 - Transformable2D.SMALL_ANGLE_TOLERANCE:
+            num_90deg = int(np.round(angle / (np.pi / 2)))
+        elif np.abs( mod_angle - np.pi / 2) < Transformable2D.SMALL_ANGLE_TOLERANCE:
             mod_angle = 0
-            num_90deg = num_90deg + 1
+            num_90deg = int(np.round(angle / (np.pi / 2)))
             is_cardinal = True
+        elif( (mod_angle >= np.pi / 2) or (mod_angle < 0) ):
+            print(angle / (np.pi / 2))
+            print(mod_angle / (np.pi / 2))
+            print(num_90deg)
+            print(Transformable2D.SMALL_ANGLE_TOLERANCE)
+
+            raise RuntimeError('mod_angle should be >=0 and < pi/2')
         else:
             is_cardinal = False
         index = (num_90deg % 4) + mirrored * 4
@@ -684,7 +696,7 @@ class Transformable2D:
             # The list above is MX followed by   ['R0','R90','R180','R270'], so
             rotate_by = Transformable2D.OrientationsWithFlip.index(orient) * np.pi / 2
             mirrored = True
-            angle = rotate_by + mod_angle
+            angle = rotate_by - mod_angle
 
         angle = angle % (2 * np.pi)
         return angle, mirrored
