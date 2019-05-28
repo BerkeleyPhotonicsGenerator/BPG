86314d1de8cc1f41fc995af82e90116388126b5e
Sidney Buchbinder <buchbinder@berkeley.edu>
2019-05-28 15:33:00 -0700

    Fixing via_stack after commit b813f1a in BAG_Framework. Do not pull this commit unless you are using a more recent version of BAG_framework than b813f1a

--- BPG/template.py
+++ BPG/template.py
@@ -839,7 +839,7 @@ class PhotonicTemplateBase(TemplateBase, metaclass=abc.ABCMeta):
 
             for via_type, weight in via_type_list:
                 try:
-                    (sp, sp2_list, sp3, dim, enc_b_list, arr_enc_b, arr_test_b) = self.grid.tech_info.get_via_drc_info(
+                    (sp, sp2_list, sp3, sp6, dim, enc_b_list, arr_enc_b, arr_test_b) = self.grid.tech_info.get_via_drc_info(
                         vname=via_name,
                         vtype=via_type,
                         mtype=bot_lay_type,
@@ -847,7 +847,7 @@ class PhotonicTemplateBase(TemplateBase, metaclass=abc.ABCMeta):
                         is_bot=True,
                     )
 
-                    (_, _, _, _, enc_t_list, arr_enc_t, arr_test_t) = self.grid.tech_info.get_via_drc_info(
+                    (_, _, _, _, _, enc_t_list, arr_enc_t, arr_test_t) = self.grid.tech_info.get_via_drc_info(
                         vname=via_name,
                         vtype=via_type,
                         mtype=top_lay_type,
@@ -971,3 +971,4 @@ class PhotonicTemplateBase(TemplateBase, metaclass=abc.ABCMeta):
                                          hidden_params={'_angle': angle},
                                          **kwargs
                                          )
+
