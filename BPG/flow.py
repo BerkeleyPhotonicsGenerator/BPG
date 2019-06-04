2d2bc2236def5a5684e60e29f5812bdf2514532d
Sidney Buchbinder <buchbinder@berkeley.edu>
2019-06-04 12:28:31 -0700

    Updating PLVS flow in BPG

--- BPG/flow.py
+++ BPG/flow.py
@@ -1,4 +1,3 @@
-
 import subprocess
 from pathlib import Path
 import logging
@@ -8,6 +7,31 @@ if TYPE_CHECKING:
     pass
 
 
+def default_callback(**kwargs):
+    """
+    Expected parameters:
+
+    Parameters
+    ----------
+    kwargs:
+        retcode : int
+            return code of the LVS process.
+        log_file : str
+            log file name.
+
+
+    Returns
+    -------
+
+    """
+    retcode = kwargs.get('retcode', True)
+    logfile = kwargs.get('logfile', None)
+    if retcode:
+        return True, logfile
+    else:
+        return False, logfile
+
+
 class Task:
 
     def __init__(self,
@@ -15,35 +39,54 @@ class Task:
                  command: list,
                  logfile: str,
                  run_dir: str,
-                 callback_function: Callable = lambda **k: None,
+                 callback_function: Callable = default_callback,
                  ):
         self.name = name
         self.command = command
         self.logfile = logfile
         self.run_dir = run_dir
-        self.callback_function = callback_function
+        if callback_function is None:
+            self.callback_function = default_callback
+        else:
+            self.callback_function = callback_function
+
+    def __repr__(self):
+        return (f'[Task Object: Name: {self.name}, '
+                f'Command: {self.command}, '
+                f'run_dir: {self.run_dir},  '
+                f'callback: {self.callback_function}]'
+                )
 
     def run(self):
         logging.info(
             f'Task started: {self.name}\n'
+            f'running command: {self.command}\n'
             f'Press <Ctrl> + C to exit.\n'
         )
 
         logfile_path = str(Path(self.run_dir) / self.logfile)
 
-        print(f'running command: {self.command}')
-
         with open(logfile_path, 'w') as f:
             retval = subprocess.run(self.command, stdout=f)
 
-        returncode = retval
-        self.callback_function(
+        callback_return = self.callback_function(
             retcode=retval.returncode,
             log_file=logfile_path
         )
-        logging.info(f'Task completed: {self.name}\n')
 
-        return retval.returncode, logfile_path
+        if isinstance(callback_return, tuple):
+            success = callback_return[0]
+        else:
+            success = callback_return
+
+        if not success:
+            logging.info(f'Task callback indicates task FAILED. See {logfile_path}')
+        else:
+            logging.info(f'Task callback indicates task passed.')
+
+        logging.info(f'Task completed: {self.name}\n\n')
+
+        return success, logfile_path
 
 
 class Flow:
@@ -64,4 +107,12 @@ class Flow:
             log_files.append(logfile)
 
         logging.info(f'Flow complete.\n'
-                     f'Log files can be found at: {log_files}')
+                     f'Log files can be found at: {log_files}\n\n')
+
+        if all(return_code for return_code in ret_codes):
+            logging.info(f'All tasks run succesfully\n\n')
+        else:
+            for ind, (code, task) in enumerate(zip(ret_codes, self._flow_list)):
+                if not code:
+                    logging.info(f'Task # {ind} FAILED:   {task}')
+            logging.info(f'\n\n')
