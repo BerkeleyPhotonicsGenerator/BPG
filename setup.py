316e89112463ec74571a51ea0272413c1d619e0f
Sidney Buchbinder <buchbinder@berkeley.edu>
2020-01-05 11:45:00 -0800

    Pinning GDSPY version because updates break syntax

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'numpy>=1.10',
         'pytest>=4',
         'matplotlib>=3',
-        'gdspy>=1.4',
+        'gdspy==1.4.3',
         'scipy>=1.1.0',
         'memory_profiler>=0.54.0',
         'Jinja2>=2.10.1',
